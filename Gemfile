55dccc6746f554031da969a9acfea326351e5ec9
JMillner <jacob.millner@outlook.com>
2015-10-07 01:01:22 +0000

    scraper is -a- scraping

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'twitter-bootswatch-rails-helpers'
 #ruby racer!
 gem 'therubyracer'
 
+gem 'mechanize'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-  #layout 'darkly'
+  require 'mechanize'
+  
   def index
+    mechanize = Mechanize.new
+    page = mechanize.get('https://www.google.com/finance/historical?q=NASDAQ%3AAAPL')
+    @title = page.title
+    @historicalData = page.search('.historical_price')
+    #@date = @date.split(' ')
   end
 end
