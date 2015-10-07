fdc207edd03c5af92615842c089a35760460c9f4
JMillner <jacob.millner@outlook.com>
2015-10-07 01:22:18 +0000

    more scraping

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,8 @@ class HomeController < ApplicationController
     page = mechanize.get('https://www.google.com/finance/historical?q=NASDAQ%3AAAPL')
     @title = page.title
     @historicalData = page.search('.historical_price')
+    @date = @historicalData.at('.bb').children[1].text
+    @historicalDates = @historicalData.at('.lm').children[0].text
     #@date = @date.split(' ')
   end
 end
