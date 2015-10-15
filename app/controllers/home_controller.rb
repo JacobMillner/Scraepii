5f40a5129e68c436138bdfb5ccf5f4cd79f0ba26
JMillner <jacob.millner@outlook.com>
2015-10-15 02:04:45 +0000

    figured out how to add library files and call their methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   require 'mechanize'
+  require 'scrapeLogic'
   
   def index
     mechanize = Mechanize.new
@@ -13,6 +14,7 @@ class HomeController < ApplicationController
       page2 = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=200')
       page3 = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=400')
     end
+    @testy = ScrapeLogic.scrapeAll('test')
     @title = page.title
     @historicalData = page.search('.historical_price')
     @date = @historicalData.at('.bb').children[1].text
---
+++ lib/scrapeLogic.rb
@@ -0,0 +1,8 @@
+class ScrapeLogic
+  require 'mechanize'
+  
+  def self.scrapeAll(symbol)
+    return symbol
+  end
+  
+end
