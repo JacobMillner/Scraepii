6b1bb68efe0c898c57fcbb6b25113605c7e10716
JMillner <jacob.millner@outlook.com>
2015-10-19 00:40:03 +0000

    scrape all fixes and a bit of cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,36 +6,12 @@ class HomeController < ApplicationController
     mechanize = Mechanize.new
     if params[:symbol] != nil
       page = mechanize.get('https://www.google.com/finance/historical?q='+ params[:symbol] + '&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=0')
-      page2 = mechanize.get('https://www.google.com/finance/historical?q='+ params[:symbol] + '&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=200')
-      page3 = mechanize.get('https://www.google.com/finance/historical?q='+ params[:symbol] + '&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=400')
-      page4 = mechanize.get('https://www.google.com/finance/historical?q='+ params[:symbol] + '&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=600')
-      page5 = mechanize.get('https://www.google.com/finance/historical?q='+ params[:symbol] + '&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=600')
+          @title = page.title
+          @priceData =ScrapeLogic.scrapeAll(params[:symbol])
     else
       #default
-      page = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=0')
-      page2 = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=200')
-      page3 = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=400')
-      page4 = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=600')
-      page5 = mechanize.get('https://www.google.com/finance/historical?cid=22144&startdate=Oct%208%2C%202013&enddate=Oct%207%2C%202015&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=600')
-    end
-    @testy = ScrapeLogic.scrapeAll(params[:symbol])
-    @title = page.title
-    @historicalData = page.search('.historical_price')
-    @date = @historicalData.at('.bb').children[1].text
-    @historicalDates = @historicalData.at('.lm').children[0].text
-    @foo = @historicalData.children[2].children[3].text
-    @count = @historicalData.children[2].count
-    @bar = @historicalData.children[2].each {|x| @stuff.push(x.text)}
-    
-   #@historicalData.children[2].each do |td|
-   #       @stuff.push(td.text.strip)
-   #end
-    @stuff = page.search('.historical_price').search('tr').map{ |n| n } #.text.strip }
-    @stuff2 = page2.search('.historical_price').search('tr').map{ |n| n } #.text.strip }
-    @stuff3 = page2.search('.historical_price').search('tr').map{ |n| n } #.text.strip }
-    #@stuff += @stuff2 + @stuff3
-    #@numbers = page3.search('.historical_price').search('tr').map{ |n| n }.count
-    
-    #@date = @date.split(' ')
+      @title = "Please Input A Symbol"
+      @priceData = []
+    end 
   end
 end
--- lib/scrapeLogic.rb
+++ lib/scrapeLogic.rb
@@ -5,14 +5,17 @@ class ScrapeLogic
   def buildGoogLink(symbol, startNum = 0)
     currDate = Time.now
     startYear = (currDate.strftime("%Y").to_i - 2).to_s #take two years off current date... I know this is ugly.
-    link = 'https://www.google.com/finance/historical?q='+ symbol + 
+    link = 'https://www.google.com/finance/historical?cid=304466804484872' +
+             '&q='+ symbol + 
              '&startdate=' + currDate.strftime('%b') +
              '%20' + currDate.strftime("%d") + 
              '%2C%20' + startYear +
              '&enddate=' + currDate.strftime('%b') +
              '%20' + currDate.strftime("%d") + 
              '%2C%20' + currDate.strftime("%Y") + 
-             '&num=200&ei=mxkcVpDiI8WFmAHYvqeQAQ&start=' + startNum
+             '&num=200&start=' + startNum.to_s
+    puts "*************"
+    puts link
     return link
   end
   
@@ -30,14 +33,15 @@ class ScrapeLogic
       
       #loop through each page until there are less than 200 records..
       #this should mean we have reached the last page
-      until recordCount >= 200 do
+      until recordCount <= 199 do
         startNum += 200
         tempGoogLink = sl.buildGoogLink(symbol, startNum)
         tempPage = mechanize.get(tempGoogLink)
         tempPrices = tempPage.search('.historical_price').search('tr').map{ |n| n }
-        recordCount = tempPrices.count
+        prices += tempPrices.drop(1)
+        recordCount = tempPrices.count    
       end
-    
+ 
     return prices
     
     #if not given a symbol return false
