e245619a3971509b0a225f01269f2fc94c12856c
JMillner <jacob.millner@outlook.com>
2015-12-11 21:53:08 +0000

    woo! we have database data! SyncAll works.

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -5,14 +5,13 @@ class SyncController < ApplicationController
     syncl = SyncLogic.new
     @stock_datum = StockDatum.find(params[:symbol])
     @days = @stock_datum.history_day.all
-    flash[:notice] = 'Sync Successful! Symbol: ' + @stock_datum.symbol + " Num of records: " + @days.count.to_s
     if @days.count > 0
-      
+      flash[:notice] = 'Sync Successful! Symbol: ' + @stock_datum.symbol + " Num of records: " + @days.count.to_s
     else
       syncl.syncOne(params[:symbol])
-      @priceData = ScrapeLogic.scrapeAll(@stock_datum.symbol)
-      @priceData = Common.prepDataForDatabase(@priceData)
-      #foreach day in
+      @stock_datum = StockDatum.find(params[:symbol])
+      @days = @stock_datum.history_day.all
+      flash[:notice] = 'Sync Successful! Symbol: ' + @stock_datum.symbol + " Num of records: " + @days.count.to_s
     end
     redirect_to stock_data_path
   end
--- lib/syncLogic.rb
+++ lib/syncLogic.rb
@@ -4,7 +4,7 @@ class SyncLogic
   require 'htmlMachine'
   require 'common'
   
-  class syncOne(symbolId)
+  def syncOne(symbolId)
     mechanize = Mechanize.new
     sl = ScrapeLogic.new
     @stock_datum = StockDatum.find(symbolId)
@@ -12,12 +12,21 @@ class SyncLogic
       @days = @stock_datum.history_day.all
       @priceData = ScrapeLogic.scrapeAll(@stock_datum.symbol)
       @priceData = Common.prepDataForDatabase(@priceData)
+      @priceData.each do |day|
+        newDay = HistoryDay.new
+        newDay.date = day.date
+        newDay.open = day.open
+        newDay.close = day.close
+        newDay.high = day.high
+        newDay.low = day.low
+        newDay.volume = day.volume
+        newDay.points = day.points
+        newDay.weekOfYear = day.weekOfYear
+        newDay.stock_datum_id = symbolId
+        newDay.save
+      end
     else
       return false
     end
-  end
-  
-  class syncAll
-  end
-  
+  end 
 end
