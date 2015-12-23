ecd88de6c30c7f326f10fd7fe26311273689d99e
Jacob Millner <jacob.millner@outlook.com>
2015-12-23 18:29:00 -0500

    Sync all finished

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -15,6 +15,17 @@ class SyncController < ApplicationController
   end
   
   def syncAll()
+    	syncl = SyncLogic.new
+	finishedMessage = "Sync Finished: "
+	symbols = StockDatum.all
+	if symbols != nil
+		symbols.each do |stock|
+			message = syncl.syncOne(stock.id)
+			finishedMessage += message + ' '
+		end
+	end
+  	flash[:notice] = finishedMessage
+  	redirect_to stock_data_path
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :stock_data
   get 'chris_sandbox/index'
   get 'sync/syncSymbol'
+  get 'sync/syncAll'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
