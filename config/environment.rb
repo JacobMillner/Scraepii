f4f6a90cfa422faac60b2aa237bbc54852ea14bc
JMillner <jacob.millner@outlook.com>
2016-01-08 01:34:51 +0000

    added git hash version to home page

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
+
 # Initialize the Rails application.
 Rails.application.initialize!
