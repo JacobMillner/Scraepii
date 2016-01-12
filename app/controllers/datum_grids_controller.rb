2b2d5d0beb00a73426918c1536686aa7dd61f259
JMillner <jacob.millner@outlook.com>
2016-01-12 01:28:49 +0000

    Screener export to csv

--- app/controllers/datum_grids_controller.rb
+++ app/controllers/datum_grids_controller.rb
@@ -1,8 +1,20 @@
 class DatumGridsController < ApplicationController
 
   def index
-    @grid = DatumGridsGrid.new(params[:datum_grids_grid]) do |scope|
-      scope.page(params[:page])
+#     @grid = DatumGridsGrid.new(params[:datum_grids_grid]) do |scope|
+#       scope.page(params[:page])
+#     end
+    @grid = DatumGridsGrid.new(params[:datum_grids_grid])
+    respond_to do |f|
+      f.html do
+        @grid.scope {|scope| scope.page(params[:page]) }
+      end
+      f.csv do
+        send_data @grid.to_csv, 
+          type: "text/csv", 
+          disposition: 'inline', 
+          filename: "grid-#{Time.now.to_s}.csv"
+      end
     end
   end
 
