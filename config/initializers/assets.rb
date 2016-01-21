6477bd2966d1b13abc4f7893d9f2d8c35f5bac6e
JMillner <jacob.millner@outlook.com>
2016-01-21 03:10:53 +0000

    Added landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += %w( highcharts.js )
 Rails.application.config.assets.precompile += %w( darkly.css )
 Rails.application.config.assets.precompile += %w( darkly.js )
 Rails.application.config.assets.precompile += %w( datagrid.css )
+Rails.application.config.assets.precompile += %w( home.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
