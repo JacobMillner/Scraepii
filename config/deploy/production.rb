f128f5c8702059ab2ea0423729efe6ad35a16af5
Jacob Millner <jacob.millner@outlook.com>
2016-03-07 20:23:53 -0500

    more production server fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-role :app, %w{deploy@uptik.io}
-role :web, %w{deploy@uptik.io}
-role :db,  %w{deploy@uptik.io}
+role :app, %w{deploy@198.211.112.29}
+role :web, %w{deploy@198.211.112.29}
+role :db,  %w{deploy@198.211.112.29}
 
 # role-based syntax
 # ==================
