c229e190d089ab0c72a776397262f5faa4b83bf7
Jacob <jacob.millner@outlook.com>
2016-06-06 15:22:04 -0400

    fixing the landing page/working on getting onReady js to play nicely with turbolinks

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,12 @@
 Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
   Rails.application.config.assets.paths << path
 end
+Dir.glob("#{Rails.root}/app/assets/javascripts/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
+Dir.glob("#{Rails.root}/app/assets/stylesheets/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
@@ -10,7 +16,27 @@ Rails.application.config.assets.precompile += %w( darkly.css )
 Rails.application.config.assets.precompile += %w( darkly.js )
 Rails.application.config.assets.precompile += %w( datagrid.css )
 Rails.application.config.assets.precompile += %w( home.css )
-Rails.application.config.assets.precompile += %w( homer/* )
+
+#homer theme
+#JS
+Rails.application.config.assets.precompile += %w( jquery-ui.min.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery.slimscroll.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( metisMenu.min.js )
+Rails.application.config.assets.precompile += %w( icheck.min.js )
+Rails.application.config.assets.precompile += %w( sprakline-index.js )
+Rails.application.config.assets.precompile += %w( homer.js )
+#css
+Rails.application.config.assets.precompile += %w( homer/font-awesome.css )
+Rails.application.config.assets.precompile += %w( homer/metisMenu.css )
+Rails.application.config.assets.precompile += %w( homer/animate.css )
+Rails.application.config.assets.precompile += %w( homer/bootstrap.css )
+Rails.application.config.assets.precompile += %w( homer/pe-icon-7-stroke.css )
+Rails.application.config.assets.precompile += %w( homer/helper.css )
+Rails.application.config.assets.precompile += %w( homer/homer-style.css )
+
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
